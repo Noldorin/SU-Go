276905c15606e610fa59007c122cd8a76d88596e
William Gross <grossw@seattleu.edu>
2019-05-26 20:44:51 -0700

    Added a ton of functionality. The actual recursive influence algorithm doesn't work as intended, but all the ancillary functions do.

--- LetsGo/LetsGo/LetsGo.cpp
+++ LetsGo/LetsGo/LetsGo.cpp
@@ -3,6 +3,8 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -20,12 +22,31 @@ struct BoardSpace
 	string last_visitor;
 	char stone;
 	char owner;
+
+	BoardSpace()
+	{
+		int x_loc = 0;
+		int y_loc = 0;
+		int white_score = 0;
+		int black_score = 0;
+		string last_visitor = "";
+		char stone = EMPTY;
+		char owner = EMPTY;
+	}
 };
 
 class GoBoard
 {
 
 private:
+
+	string ToLastVisitor(int x, int y)
+	{
+		ostringstream oss;
+
+		oss << x << y;
+		return oss.str();
+	}
 
 	void InitBoard(BoardSpace** board)
 	{
@@ -41,14 +62,51 @@ private:
 		{
 			for (unsigned int y = 1; y < BoardSize - 1; y++)
 			{
-				MyBoard[x][y].stone = EMPTY;
+				board[x][y].x_loc = 0;
+				board[x][y].y_loc = 0;
+				board[x][y].white_score = 0;
+				board[x][y].black_score = 0;
+				board[x][y].last_visitor = "";
+				board[x][y].stone = EMPTY;
+				board[x][y].owner = EMPTY;
 			}
 		}
 	}
 
+
+
+	void ExertInfluence(int cur_x, int cur_y, int orig_x, int orig_y, int cur_inf, char stone)
+	{
+		string mylastVis = ToLastVisitor(orig_x, orig_y);
+
+		if ((cur_inf > 0) &&
+			(MyBoard[cur_x][cur_y].last_visitor.compare(mylastVis) != 0) &&
+			(MyBoard[cur_x][cur_y].stone == EMPTY) || (cur_x == orig_x && cur_y == orig_y))
+		{
+			if (stone == BLACK)
+			{
+				MyBoard[cur_x][cur_y].black_score += cur_inf;
+			}
+			else if (stone == WHITE)
+			{
+				MyBoard[cur_x][cur_y].white_score += cur_inf;
+			}
+			MyBoard[cur_x][cur_y].last_visitor = ToLastVisitor(orig_x, orig_y);
+
+			ExertInfluence(cur_x + 1, cur_y, orig_x, orig_y, cur_inf - 1, stone);
+			ExertInfluence(cur_x - 1, cur_y, orig_x, orig_y, cur_inf - 1, stone);
+			ExertInfluence(cur_x, cur_y - 1, orig_x, orig_y, cur_inf - 1, stone);
+			ExertInfluence(cur_x, cur_y + 1, orig_x, orig_y, cur_inf - 1, stone);
+		}
+		return;
+	}
+	 
 public:
 
 	unsigned int BoardSize;
+	unsigned int total_white_score;
+	unsigned int total_black_score;
+
 	BoardSpace **MyBoard;
 
 	GoBoard(int size)
@@ -87,6 +145,46 @@ public:
 		cout << endl;
 	}
 
+	void PrintInfluence()
+	{
+		for (unsigned int x = 0; x < BoardSize; x++)
+		{
+			for (unsigned int y = 0; y < BoardSize; y++)
+			{
+				if (MyBoard[x][y].stone != EDGE)
+				{
+					cout << MyBoard[x][y].owner << ' ';
+				}
+				else
+				{
+					cout << EDGE << ' ';
+				}
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
+	void PrintScores()
+	{
+		for (unsigned int x = 0; x < BoardSize; x++)
+		{
+			for (unsigned int y = 0; y < BoardSize; y++)
+			{
+				if (MyBoard[x][y].stone != EDGE)
+				{
+					cout << MyBoard[x][y].black_score << "   ";
+				}
+				else
+				{
+					cout << EDGE << "   ";
+				}
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
 	bool PlaceStone(int x, int y, char newStone)
 	{
 		if (MyBoard[x][y].stone == EMPTY)
@@ -100,9 +198,54 @@ public:
 		return true;
 	}
 
-	void ExertInfluence(int cur_x, int cur_y, int orig_x, int orig_y, char stone)
+	void CalculateInfluence()
 	{
-		int north_x, south_x, east_x, west_x, north_y, south_y, east_y, west_y;
+		total_black_score = 0;
+		total_white_score = 0;
+
+		for (unsigned int x = 1; x < BoardSize - 1; x++)
+		{
+			for (unsigned int y = 1; y < BoardSize - 1; y++)
+			{
+				MyBoard[x][y].white_score = 0;
+				MyBoard[x][y].black_score = 0;
+			}
+		}
+		for (unsigned int x = 1; x < BoardSize - 1; x++)
+		{
+			for (unsigned int y = 1; y < BoardSize - 1; y++)
+			{
+				if (MyBoard[x][y].stone == BLACK)
+				{
+					ExertInfluence(x, y, x, y, BoardSize - 2, BLACK);
+				}
+				else if (MyBoard[x][y].stone == WHITE)
+				{
+					ExertInfluence(x, y, x, y, BoardSize - 2, WHITE);
+				}
+			}
+		}
+		for (unsigned int x = 1; x < BoardSize - 1; x++)
+		{
+			for (unsigned int y = 1; y < BoardSize - 1; y++)
+			{
+				if (MyBoard[x][y].stone == EMPTY)
+				{
+					if (MyBoard[x][y].white_score < MyBoard[x][y].black_score)
+					{
+						MyBoard[x][y].owner = BLACK;
+					}
+					else if (MyBoard[x][y].white_score > MyBoard[x][y].black_score)
+					{
+						MyBoard[x][y].owner = WHITE;
+					}
+					else
+					{
+						MyBoard[x][y].owner = EMPTY;
+					}
+				}
+			}
+		}
 	}
 };
 
@@ -129,8 +272,11 @@ int main()
 			cout << "That is an illegal move... no stone placed." << endl;
 		}
 
-		cout << endl;
+		cout << endl;		
+		MyGoBoard.PrintInfluence();
 		MyGoBoard.PrintBoard();
+		MyGoBoard.CalculateInfluence();
+		MyGoBoard.PrintInfluence();
 
 		cout << "Would you like to keep playing? y/n : ";
 		cin >> input_char;
